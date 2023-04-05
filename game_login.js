e3ff58e4c4043ca4f45ddd4cc5776f4453053901
Romeo0Rock <116769162+Romeo0Rock@users.noreply.github.com>
2023-04-05 12:19:28 -0400

    Add files via upload

---
+++ game_login.js
@@ -0,0 +1,15 @@
+
+function addUser() {
+  player1_name = document.getElementById("player1_name_input").value;
+  player2_name = document.getElementById("player2_name_input").value;
+
+    //Set "player1_name" using localStorage.setItem() function
+	  //Set "player2_name" using localStorage.setItem() function
+
+ localStorage.setItem()
+ localStorage.setItem()
+
+
+    window.location = "game_page.html";
+}
+
